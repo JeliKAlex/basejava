5aad8f8d5a3b436d08669dbade693aac22658d17
JeliKAlex <kisterev0a0a@gmail.com>
2024-10-13 19:09:00 +0700

    Refactoring(removed duplicates)

--- src/DeadlockExample.java
+++ src/DeadlockExample.java
@@ -3,39 +3,43 @@ public class DeadlockExample {
     private static final Object resource2 = new Object();
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
+        Runnable procces1 = startProcces(resource1, resource2, "Thread 1");
+        Runnable procces2 = startProcces(resource2, resource1, "Thread 2");
+
+        Thread thread1 = new Thread(procces1);
+        Thread thread2 = new Thread(procces2);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Эта строка не должна быть достигнута из-за взаимной блокировки.");
+    }
+
+    private static Runnable startProcces(Object resource1, Object resource2, String thread) {
+        return () -> {
             synchronized (resource1) {
-                System.out.println("Thread 1: удерживает resource 1.");
+                System.out.println(thread + ": удерживает " + resource1 + ".");
 
                 try {
                     Thread.sleep(50);
-                } catch (InterruptedException e) {};
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                ;
 
-                System.out.println("Thread 1: ожидает resource 2.");
+                System.out.println(thread + ": ожидает " + resource2 + ".");
 
                 synchronized (resource2) {
-                    System.out.println("Thread 1: удерживает resource 1 и 2.");
+                    System.out.println(thread + ": удерживает resources.");
                 }
             }
-        });
-
-         Thread thread2 = new Thread(() -> {
-             synchronized (resource2) {
-                 System.out.println("Thread 2: удерживает resource 2.");
-
-                 try {
-                     Thread.sleep(50);
-                 } catch (InterruptedException e) {};
-
-                 System.out.println("Thread 2: ожидает resource 1.");
-
-                 synchronized (resource1) {
-                     System.out.println("Thread 1: удерживает resource 2 и 1.");
-                 }
-             }
-         });
-
-         thread1.start();
-         thread2.start();
+        };
     }
 }
